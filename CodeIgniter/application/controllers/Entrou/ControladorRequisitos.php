5b0d916bfa2c45c6ba4a5c6b9f03b6e9b0d5d8cf
alunos <joaopauloff7@hotmail.com>
2017-06-29 13:58:49 -0300

    Telas de confirmação adicionadas.

--- CodeIgniter/application/controllers/Entrou/ControladorRequisitos.php
+++ CodeIgniter/application/controllers/Entrou/ControladorRequisitos.php
@@ -159,13 +159,13 @@ class ControladorRequisitos extends CI_Controller {
 	 public function criadoSubs($idform, $id){
 	 		$confuser = $this->ControladorModel->getProfessor($id);
 	 		$this->mandarEmailConfProf($idform, $confuser->email);
-		    $this->load->view('Entrou/requisicaoEfetuada');
+		    $this->load->view('Entrou/confirmacaoEfetuada');
     }
     
     public function criadoVis($id){
 	 		$confuser = $this->ControladorModel->getCoorVis($id);
 	 		$this->mandarEmailConfCoorVis($id, $confuser->email);
-		    $this->load->view('Entrou/requisicaoEfetuada');
+		    $this->load->view('Entrou/confirmacaoEfetuada');
     }
     
     
@@ -179,7 +179,7 @@ class ControladorRequisitos extends CI_Controller {
 	 		$this->ControladorModel->setProfessor($idform, $this->session->userdata('login'));
 	 		$confuser = $this->ControladorModel->getCoorSubs($idform);
 	 		$this->mandarEmailConfCoorSubs($idform, $confuser->email);
-	 		$this->load->view('Entrou/requisicaoEfetuada');
+	 		$this->load->view('Entrou/confirmacaoEfetuada');
     }
     
     
@@ -191,7 +191,7 @@ class ControladorRequisitos extends CI_Controller {
 	
     public function confirmarCoorSubs0($idform){
 	 		$this->ControladorModel->setCoorSubs($idform, $this->session->userdata('login'));
-			$this->load->view('Entrou/requisicaoEfetuada');
+			$this->load->view('Entrou/confirmacaoEfetuada');
 	 		  
     }
     
@@ -206,7 +206,7 @@ class ControladorRequisitos extends CI_Controller {
 	 		$this->ControladorModel->setCoorVis($idform, $this->session->userdata('login'));
 	 		$confuser = $this->ControladorModel->getDirVis($idform);
 	 		$this->mandarEmailConfDirVis($idform, $confuser->email);
-	 		$this->load->view('Entrou/requisicaoEfetuada');
+	 		$this->load->view('Entrou/confirmacaoEfetuada');
     }
     
   
@@ -218,7 +218,7 @@ class ControladorRequisitos extends CI_Controller {
 	
 	public function confirmarDirVis0($idform){
 	 		$this->ControladorModel->setDirVis($idform, $this->session->userdata('login'));
-	 		$this->load->view('Entrou/requisicaoEfetuada');
+	 		$this->load->view('Entrou/confirmacaoEfetuada');
     }
  }
  
